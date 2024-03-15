17bb8ce752b734428c9c48d8e4a1a0f1e48a4f0b
Matthew (mjmfighter) <mjmjelde+mjmfighter@gmail.com>
2024-03-14 21:37:26 -0500

    Added 7d2d yolk

---
+++ 7d2d/entrypoint2.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Wrapper around entrypoint.sh.  This just copies logs/latest.log.0 to logs/latest.log.1, and logs/latest.log to logs/latest.log.0, then runs entrypoint.sh.
+
+set -e
+
+cd /home/container || exit 1
+
+if [ -f logs/latest.log.0 ]; then
+  cp logs/latest.log.0 logs/latest.log.1
+fi
+if [ -f logs/latest.log ]; then
+  cp logs/latest.log logs/latest.log.0
+fi
+
+exec /home/container/entrypoint.sh "$@"
