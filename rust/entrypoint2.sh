2c596f0489036ebc930c8be90109b0986f7e23a5
Matthew Mjelde <mjmjelde@tamu.edu>
2024-04-03 14:53:59 -0500

    Update Dockerfile and entrypoint2.sh

--- rust/entrypoint2.sh
+++ rust/entrypoint2.sh
@@ -12,21 +12,21 @@ if [ -f latest.log ]; then
   cp latest.log latest.log.0
 fi
 
-# If GITHUB_URL and GITHUB_PRIVATE_KEY are set, we'll use them to clone the repository to /home/container/repo (current user is container)
+# If GITHUB_URL and GITHUB_PRIVATE_KEY are set, we'll use them to clone the repository to /tmp/repo (current user is container)
 if [ -n "$GITHUB_URL" ] && [ -n "$GITHUB_PRIVATE_KEY" ]; then
   echo "Cloning repository from $GITHUB_URL"
-  mkdir -p /home/container/repo
+  mkdir -p /tmp/repo
   mkdir -p /home/container/.ssh
-  ssh-keyscan github.com >> /home/container/.ssh/known_hosts
+  # ssh-keyscan github.com >> /home/container/.ssh/known_hosts
   echo "$GITHUB_PRIVATE_KEY" > /home/container/.ssh/id_rsa
   chmod 600 /home/container/.ssh/id_rsa
-  # Clone the repository to /home/container/repo using GITHUB_BRANCH if it's set
+  # Clone the repository to /tmp/repo using GITHUB_BRANCH if it's set
   if [ -n "$GITHUB_BRANCH" ]; then
-    git clone --single-branch --branch "$GITHUB_BRANCH" "$GITHUB_URL" /home/container/repo
+    git clone --single-branch --branch "$GITHUB_BRANCH" "$GITHUB_URL" /tmp/repo
   else
-    git clone "$GITHUB_URL" /home/container/repo
+    git clone "$GITHUB_URL" /tmp/repo
   fi
-  cd /home/container/repo || exit 1
+  cd /tmp/repo || exit 1
 
   # If GITHUB_FILE_POSTFIX is set, look for any files with that postfix and remove that postfix using find
   if [ -n "$GITHUB_FILE_POSTFIX" ]; then
@@ -34,12 +34,12 @@ if [ -n "$GITHUB_URL" ] && [ -n "$GITHUB_PRIVATE_KEY" ]; then
     find . -type f -name "*$GITHUB_FILE_POSTFIX" -exec bash -c 'mv "$1" "${1%$2}"' _ {} "$GITHUB_FILE_POSTFIX" \;
   fi
 
-  # Now rsync the files from /home/container/repo to /home/container
-  echo "Copying files from /home/container/repo to /home/container"
-  rsync -a --exclude=".git" /home/container/repo/ /home/container/
+  # Now rsync the files from /tmp/repo to /home/container
+  echo "Copying files from /tmp/repo to /home/container"
+  rsync -a --exclude=".git" /tmp/repo/ /home/container/
 
   # Clean up the repository and ssh keys
-  rm -rf /home/container/repo
+  rm -rf /tmp/repo
   rm -rf /home/container/.ssh
 fi
 
