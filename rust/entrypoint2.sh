3f7bc30fe5aeb386913449e10912765c7c8bf3a5
Matthew Mjelde <mjmjelde@tamu.edu>
2024-04-03 14:09:37 -0500

    Update repository cloning directory

--- rust/entrypoint2.sh
+++ rust/entrypoint2.sh
@@ -12,21 +12,21 @@ if [ -f latest.log ]; then
   cp latest.log latest.log.0
 fi
 
-# If GITHUB_URL and GITHUB_PRIVATE_KEY are set, we'll use them to clone the repository to /tmp/repo (current user is container)
+# If GITHUB_URL and GITHUB_PRIVATE_KEY are set, we'll use them to clone the repository to /home/container/repo (current user is container)
 if [ -n "$GITHUB_URL" ] && [ -n "$GITHUB_PRIVATE_KEY" ]; then
   echo "Cloning repository from $GITHUB_URL"
-  mkdir -p /tmp/repo
+  mkdir -p /home/container/repo
   mkdir -p /home/container/.ssh
   ssh-keyscan github.com >> /home/container/.ssh/known_hosts
   echo "$GITHUB_PRIVATE_KEY" > /home/container/.ssh/id_rsa
   chmod 600 /home/container/.ssh/id_rsa
-  # Clone the repository to /tmp/repo using GITHUB_BRANCH if it's set
+  # Clone the repository to /home/container/repo using GITHUB_BRANCH if it's set
   if [ -n "$GITHUB_BRANCH" ]; then
-    git clone --single-branch --branch "$GITHUB_BRANCH" "$GITHUB_URL" /tmp/repo
+    git clone --single-branch --branch "$GITHUB_BRANCH" "$GITHUB_URL" /home/container/repo
   else
-    git clone "$GITHUB_URL" /tmp/repo
+    git clone "$GITHUB_URL" /home/container/repo
   fi
-  cd /tmp/repo || exit 1
+  cd /home/container/repo || exit 1
 
   # If GITHUB_FILE_POSTFIX is set, look for any files with that postfix and remove that postfix using find
   if [ -n "$GITHUB_FILE_POSTFIX" ]; then
@@ -34,9 +34,9 @@ if [ -n "$GITHUB_URL" ] && [ -n "$GITHUB_PRIVATE_KEY" ]; then
     find . -type f -name "*$GITHUB_FILE_POSTFIX" -exec bash -c 'mv "$1" "${1%$2}"' _ {} "$GITHUB_FILE_POSTFIX" \;
   fi
 
-  # Now rsync the files from /tmp/repo to /home/container
-  echo "Copying files from /tmp/repo to /home/container"
-  rsync -a --exclude=".git" /tmp/repo/ /home/container/
+  # Now rsync the files from /home/container/repo to /home/container
+  echo "Copying files from /home/container/repo to /home/container"
+  rsync -a --exclude=".git" /home/container/repo/ /home/container/
 fi
 
 exec /entrypoint.sh "$@"
